27b4b45f8f853881de1e39c8439671503aae0b49
Francisco <francisco.alles@gmail.com>
2021-05-25 20:45:12 -0300

    commit

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "oci_core_vcn" "internal" {
   compartment_id = "ocid1.tenancy.oc1..aaaaaaaaiq4zp55favmeitmdd4c3lggs57iswzowvwa5k6566whocrhxgbsa"
   display_name   = "My internal VCN"
 }
+
+
+resource "oci_core_subnet" "dev" {
+  vcn_id                      = oci_core_vcn.internal.id
+  cidr_block                  = "172.16.0.0/24"
+  compartment_id = "ocid1.tenancy.oc1..aaaaaaaaiq4zp55favmeitmdd4c3lggs57iswzowvwa5k6566whocrhxgbsa"
+  display_name                = "Dev subnet 1"
+  prohibit_public_ip_on_vnic  = true
+  dns_label                   = "dev"
+}
