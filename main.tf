f8d705aacfb2c5774557bca76cd88f313ab5f330
Francisco <francisco.alles@gmail.com>
2021-05-25 20:49:27 -0300

    commit

--- main.tf
+++ main.tf
@@ -12,13 +12,14 @@ provider "oci" {
   config_file_profile = "learn-terraform"
 }
 
+/*
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "172.16.0.0/20"
   compartment_id = "ocid1.tenancy.oc1..aaaaaaaaiq4zp55favmeitmdd4c3lggs57iswzowvwa5k6566whocrhxgbsa"
   display_name   = "My internal VCN"
 }
-
+*/
 
 resource "oci_core_subnet" "dev" {
   vcn_id                      = oci_core_vcn.internal.id
