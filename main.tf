ffbdff62da5a6c93df7ebc5a7acab7c97ef2f20d
Francisco <francisco.alles@gmail.com>
2021-05-25 21:14:03 -0300

    commit

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "oci_core_vcn" "internal" {
 resource "oci_core_subnet" "dev" {
   vcn_id                      = oci_core_vcn.internal.id
   cidr_block                  = "172.16.0.0/24"
-  compartment_id = var.compartment_id
+  compartment_id              = var.compartment_id
   display_name                = "Dev Subnet"
   prohibit_public_ip_on_vnic  = true
   dns_label                   = "dev"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
-compartment_id   = "ocid1.tenancy.oc1..aaaaaaaiq4zp55favmeitmdd4c3lggs57iswzowvwa5k6566whocrhxgbsa"
+compartment_id   = "ocid1.tenancy.oc1..aaaaaaaaiq4zp55favmeitmdd4c3lggs57iswzowvwa5k6566whocrhxgbsa"
 region           = "sa-santiago-1"
